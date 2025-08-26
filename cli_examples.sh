datamass-io/bda2025-winter-lab4

python main: initial commit
In this commit we add the following files:
- main.py
- requirements.txt
- cli_examples.sh
- .env

cli_examples.sh
@@ -1,0 +1,19 @@
+#!/bin/zsh
+
+# create the database account
+az cosmosdb create --name mdwintertest4321 --kind GlobalDocumentDB --resource-group DataAcademyWinter2025
+
+# list accounts (shows document endpoint uri)
+az cosmosdb list -g DataAcademyWinter2025 -o table
+
+# create the database
+az cosmosdb sql database create --account-name mdwintertest4321 --name "iot" --resource-group DataAcademyWinter2025
+
+# create the container
+az cosmosdb sql container create --account-name mdwintertest4321 --database-name "iot" --name "monitoring" --partition-key-path "/id" --throughput 400 --resource-group DataAcademyWinter2025
+
+# show the database keys
+az cosmosdb keys list --name mdwintertest4321 --resource-group DataAcademyWinter2025
+
+# show the database connection strings
+az cosmosdb keys list --type connection-strings --name mdwintertest4321 --resource-group DataAcademyWinter2025

main.py
@@ -1,0 +1,67 @@
+import os
+import json
+import uuid
+
+import psutil
+import time
+
+from azure.identity import DefaultAzureCredential
+from azure.cosmos import CosmosClient
+
+# for database management use azure-mgmt-cosmosdb
+
+database_name = "iot"
+container_name = "monitoring"
+database_endpoint = "https://mdtest4321.documents.azure.com:443/"
+try:
+    print("Hello, Azure CosmosDB!")
+    # credential = DefaultAzureCredential()
+    # client = CosmosClient(url=database_endpoint, credential=credential)
+
+    # or use connection string
+    cosmos_key = os.getenv('COSMOS_KEY')
+    client = CosmosClient(url=database_endpoint, credential=cosmos_key)
+
+    database = None  # TODO create database client
+    container = None  # TODO create container client
+    ID = str(uuid.uuid4())
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    data = {
+        "id": ID,
+        "system": "home",  # try to create data for other system, e.g. "work"
+        "timestamp": timestamp,
+        "cpu_usage": cpu,
+        "mem_usage": mem,
+    }
+
+    print("Creating item...")
+    # TODO create the item
+
+    # OR QUERY WITH @id parameter (PK)
+    print("Querying item...")
+    QUERY = None  # TODO write the SQL QUERY WITH @id parameter
+    params = [dict(name="@id", value=ID)]
+    results = None  # TODO query the container, use parameters, experiment with enable_cross_partition_query flag
+
+    items = [item for item in results]
+    output = json.dumps(items, indent=True)
+    print("Result list\t", output)
+
+
+    # Query by system property instead of id (non-PK)
+    print("Querying item by system property...")
+    QUERY = None  # TODO write the SQL QUERY WITH @system parameter
+    SYSTEM = "home"
+    params = [dict(name="@system", value=SYSTEM)]
+    results = None  # TODO query the container, use parameters, experiment with enable_cross_partition_query flag
+
+    items = [item for item in results]
+    output = json.dumps(items, indent=True)
+    print("Result list\t", output)
+
+
+except Exception as ex:
+    print("Exception: ")
+    print(ex)

afung-dev/lab4

cli_examples.sh
@@ -1,0 +1,19 @@
+#!/bin/zsh
+
+# create the database account
+az cosmosdb create --name mdsummer2025 --kind GlobalDocumentDB --resource-group DataAcademySummer2025
+
+# list accounts (shows document endpoint uri)
+az cosmosdb list -g DataAcademySummer2025 -o table
+
+# create the database
+az cosmosdb sql database create --account-name mdsummer2025 --name "iot" --resource-group DataAcademySummer2025
+
+# create the container
+az cosmosdb sql container create --account-name mdsummer2025 --database-name "iot" --name "monitoring" --partition-key-path "/id" --throughput 400 --resource-group DataAcademySummer2025
+
+# show the database keys
+az cosmosdb keys list --name mdsummer2025 --resource-group DataAcademySummer2025
+
+# show the database connection strings
+az cosmosdb keys list --type connection-strings --name mdsummer2025 --resource-group DataAcademySummer2025

main.py
@@ -1,0 +1,67 @@
+import os
+import json
+import uuid
+
+import psutil
+import time
+
+from azure.identity import DefaultAzureCredential
+from azure.cosmos import CosmosClient
+
+# for database management use azure-mgmt-cosmosdb
+
+database_name = "iot"
+container_name = "monitoring"
+database_endpoint = "https://mdsummer2025.documents.azure.com:443/"
+try:
+    print("Hello, Azure CosmosDB!")
+    # credential = DefaultAzureCredential()
+    # client = CosmosClient(url=database_endpoint, credential=credential)
+
+    # or use connection string
+    cosmos_key = os.getenv('COSMOS_KEY')
+    client = CosmosClient(url=database_endpoint, credential=cosmos_key)
+
+    database = None  # TODO create database client
+    container = None  # TODO create container client
+    ID = str(uuid.uuid4())
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    cpu = psutil.cpu_percent()
+    mem = psutil.virtual_memory().percent
+    data = {
+        "id": ID,
+        "system": "home",  # try to create data for other system, e.g. "work"
+        "timestamp": timestamp,
+        "cpu_usage": cpu,
+        "mem_usage": mem,
+    }
+
+    print("Creating item...")
+    # TODO create the item
+
+    # OR QUERY WITH @id parameter (PK)
+    print("Querying item...")
+    QUERY = None  # TODO write the SQL QUERY WITH @id parameter
+    params = [dict(name="@id", value=ID)]
+    results = None  # TODO query the container, use parameters, experiment with enable_cross_partition_query flag
+
+    items = [item for item in results]
+    output = json.dumps(items, indent=True)
+    print("Result list\t", output)
+
+
+    # Query by system property instead of id (non-PK)
+    print("Querying item by system property...")
+    QUERY = None  # TODO write the SQL QUERY WITH @system parameter
+    SYSTEM = "home"
+    params = [dict(name="@system", value=SYSTEM)]
+    results = None  # TODO query the container, use parameters, experiment with enable_cross_partition_query flag
+
+    items = [item for item in results]
+    output = json.dumps(items, indent=True)
+    print("Result list\t", output)
+
+
+except Exception as ex:
+    print("Exception: ")
+    print(ex)